ad349e5aee9343771a8eef26f7061665946be7b5
eduardo dias <ediaz@intelisis.com.mx>
2024-03-07 13:45:40 -0600

    hasta la seccion 14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,8 +15,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+
     AppRoutingModule,
-    SharedModule
+    SharedModule,
   ],
   providers: [
     provideAnimationsAsync()
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
+
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
-import { MaterialModule } from '../material/material.module';
+
 
 
 @NgModule({
@@ -15,9 +17,9 @@ import { MaterialModule } from '../material/material.module';
     RegisterPageComponent
   ],
   imports: [
+    MaterialModule,
     CommonModule,
     AuthRoutingModule,
-    MaterialModule
   ]
 })
 export class AuthModule { }
